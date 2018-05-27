510b345e74859ff2ae5b99bc31fb6d121b7cfdf0
Abhishek Raj <rai.abhishekraj@gmail.com>
2018-05-27 18:12:48 +0530

    try ends

--- js/model.js
+++ js/model.js
@@ -5,8 +5,8 @@ app.factory("finderFactory", function ($http, $q) {
     var object = {};
     object.callServer = function (playerId) {
         var defered = $q.defer();
-        //var url = "https://cricapi.com/api/playerStats?pid=" + playerId + "&&apikey=MzRo95088EUzvTTW2snM0Kxxrii1";
-        var url = "http://cricapi.com/api/matches&&apikey=MzRo95088EUzvTTW2snM0Kxxrii1";
+        var url = "https://cricapi.com/api/playerStats?pid=" + playerId + "&&apikey=MzRo95088EUzvTTW2snM0Kxxrii1";
+        //var url = "http://cricapi.com/api/matches&&apikey=MzRo95088EUzvTTW2snM0Kxxrii1";
         $http.get(url).then(success, fail);
 
         function success(data) {
